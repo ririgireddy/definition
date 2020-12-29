94a99cc3d99ae2c6f660d02f42004c3f9e252b13
ririgireddy <64945504+ririgireddy@users.noreply.github.com>
2020-12-28 22:55:19 -0800

    Add files via upload

---
+++ definition.py
@@ -0,0 +1,26 @@
+import re
+import urllib.request
+import sys
+
+try: color = sys.stdout.shell
+except AttributeError: raise RuntimeError("Use IDLE")
+
+while True:
+    url="https://www.dictionary.com/browse/"
+    word=input("Enter your word: ")
+
+    url=url+word
+
+    data=urllib.request.urlopen(url).read()
+    data1=data.decode("utf-8")
+
+    m=re.search('<meta name="description" content=', data1)
+    start=m.end()+1
+    end=m.end()+200
+    string=data1[start:end]
+
+    m=re.search('See more.">', string)
+    start=0
+    end=m.start()
+    final=string[0:end]
+    color.write(final+"\n","STRING")
